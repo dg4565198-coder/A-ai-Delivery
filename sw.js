2c8e74e7c1d38aa8dc224a240dd221f3db97aacf
Rotta do Acai <rotta@acai.app>
2026-09-20 15:00:11 -0300

    fix: resolve unclosed div tag in success-modal isolating all bottom modals (v28)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
- * ROTTA DO AÇAÍ - SERVICE WORKER (v27)
+ * ROTTA DO AÇAÍ - SERVICE WORKER (v28)
  * Background Order Tracking & Push Notification Engine
  */
 
-const CACHE_NAME = 'rotta-acai-v27';
+const CACHE_NAME = 'rotta-acai-v28';
 const urlsToCache = [
   './',
   './index.html',
