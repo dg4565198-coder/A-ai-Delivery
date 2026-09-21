c19a3d2c151f32e1a8e6826cf13da93faf4f57e6
Rotta do Acai <rotta@acai.app>
2026-09-20 21:44:57 -0300

    fix: adiciona manipulador inline para garantir abertura do modal de instalacao do app (v41)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /**
- * ROTTA DO AÇAÍ - SERVICE WORKER (v40)
+ * ROTTA DO AÇAÍ - SERVICE WORKER (v41)
  * Background Order Tracking & Push Notification Engine
  */
 
-const CACHE_NAME = 'rotta-acai-v40';
+const CACHE_NAME = 'rotta-acai-v41';
 const urlsToCache = [
   './',
   './index.html',
